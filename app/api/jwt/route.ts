26394d67b1d5ac6ed3f99e958565d0353af733b0
Manish Kukreja <manish.kukreja@snowflake.com>
2025-03-08 20:52:29 +1300

    feat: dockerised app

--- app/api/jwt/route.ts
+++ app/api/jwt/route.ts
@@ -1,26 +1,23 @@
 import { NextResponse } from 'next/server';
-import { createHash, createPrivateKey, createPublicKey } from 'crypto';
+import { createHash, createPrivateKey, createPublicKey, KeyObject } from 'crypto';
 import jwt from 'jsonwebtoken';
 import fs from 'fs';
 import path from 'path';
-import { execSync } from 'child_process';
 
 function getRSAKey(): Buffer {
     return fs.readFileSync(path.join(process.cwd(), 'rsa_key.p8'));
 }
 
-function getDecryptedKey(passphrase: string): string {
-    const keyPath = path.join(process.cwd(), 'rsa_key.p8');
-    
+function getDecryptedKey(key: string, passphrase: string): KeyObject {
     try {
-        return execSync(`openssl pkcs8 -in ${keyPath} -passin pass:${passphrase} -nocrypt`, { encoding: 'utf8' });
+        const privateKey = createPrivateKey({
+            key: key,
+            format: 'pem',
+            passphrase: passphrase,
+        });
+        return privateKey;
     } catch (error) {
-        try {
-            return execSync(`openssl rsa -in ${keyPath} -passin pass:${passphrase}`, { encoding: 'utf8' });
-        }
-        catch (error) {
-            throw new Error("Failed to decrypt private key: " + (error as Error).message);
-        }
+        throw new Error("Failed to decrypt private key: " + (error as Error).message);
     }
 }
 
@@ -28,15 +25,14 @@ export async function GET() {
     const SNOWFLAKE_RSA_PASSPHRASE = process.env.SNOWFLAKE_RSA_PASSPHRASE ?? '';
     const SNOWFLAKE_RSA_KEY = process.env.SNOWFLAKE_RSA_KEY ?? '';
     try {
-        let rsaKey: string | Buffer;
+        let rsaKey: KeyObject;
         if (SNOWFLAKE_RSA_PASSPHRASE !== '' && SNOWFLAKE_RSA_KEY !== '') {
-            rsaKey = getDecryptedKey(SNOWFLAKE_RSA_PASSPHRASE)
+            rsaKey = getDecryptedKey(SNOWFLAKE_RSA_KEY, SNOWFLAKE_RSA_PASSPHRASE);
         } else {
-            rsaKey = getRSAKey();
+            rsaKey = createPrivateKey(getRSAKey());
         }
         
-        const privateKey = createPrivateKey(rsaKey);
-        const publicKey = createPublicKey(privateKey);
+        const publicKey = createPublicKey(rsaKey);
         const publicKeyRaw = publicKey.export({ type: 'spki', format: 'der' });
 
         const sha256Hash = createHash('sha256').update(publicKeyRaw).digest('base64');
@@ -57,7 +53,7 @@ export async function GET() {
             exp: nowInSeconds + oneHourInSeconds,
         };
 
-        const token = jwt.sign(payload, rsaKey.toString(), { algorithm: 'RS256' });
+        const token = jwt.sign(payload, rsaKey.export({ format: 'pem', type: 'pkcs8' }).toString(), { algorithm: 'RS256' });
 
         return NextResponse.json({
             token: {
